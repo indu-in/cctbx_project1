40893446fdbcfc3191b153829e0e0bfa483a27b7
Billy K. Poon <bkpoon@lbl.gov>
2019-10-04 16:49:20 -0700

    Fix another typo

--- iotbx/command_line/mtz_as_cif.py
+++ iotbx/command_line/mtz_as_cif.py
@@ -268,7 +268,7 @@ class mtz_as_cif_blocks(object):
     for data_type in data_types:
       print("MORE")
       print(data_type)
-      print(self.cif_block[data_type])
+      print(self.cif_blocks[data_type])
       print(r_free_xray)
       print(r_free_neutron)
       print("LESS")
