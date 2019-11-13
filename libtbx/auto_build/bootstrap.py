f90defe4dbd9f57d218749a4d450fd9a2c79a5d7
Billy K. Poon <bkpoon@lbl.gov>
2019-11-12 16:39:30 -0800

    bootstrap: conda updates
    - Check self.use_conda against None
    - Boost, eigen, and scons are available as conda packages

--- libtbx/auto_build/bootstrap.py
+++ libtbx/auto_build/bootstrap.py
@@ -1104,12 +1104,27 @@ class Builder(object):
 
     # Add 'hot' sources
     if hot:
-      list(map(self.add_module, self.get_hot()))
+      # conda builds do not need eigen, scons
+      hot = self.get_hot()
+      if self.use_conda is not None:
+        for module in ['eigen', 'scons']:
+          try:
+            hot.remove(module)
+          except ValueError:
+            pass
+      list(map(self.add_module, hot))
 
     # Add svn sources.
     self.revert=revert
     if update:
-      list(map(self.add_module, self.get_codebases()))
+      # conda builds do not need boost
+      codebases = self.get_codebases()
+      if self.use_conda is not None:
+        try:
+          codebases.remove('boost')
+        except ValueError:
+          pass
+      list(map(self.add_module, codebases))
 
     # always remove .pyc files
     self.remove_pyc()
